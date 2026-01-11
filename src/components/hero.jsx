00a0a08c3962c611ad98b52000ea804736ba545d
Music-Genre_and-Artist-Prediction <mayurkale@gmail.com>
2026-01-11 19:05:38 +0530

    Updated Name

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -26,12 +26,12 @@ const Hero = () => {
               animate="visible"
               className="pb-10 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl"
             >
-              Mayur Kale
+              Mayur Dadasaheb Kale
             </motion.h1>
             <motion.span variants={Container(0.6)}
             initial="hidden"
             animate="visible" className="bg-grading-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent-200"> 
-              Full Stack Developer
+              Python Developer | Full Stack Developer | AI&ML Engineer
             </motion.span>
             <motion.p variants={Container(1  )}
             initial="hidden"
